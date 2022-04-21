0dbf4752ab2ce6ba6615c1b1752712bb56898660
NIcolas Cañete <ncanete@trust.lat>
2022-04-21 13:40:23 -0400

    add contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FormularioContactoComponent } from './formulario-contacto/formulario-contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FormularioContactoComponent
   ],
   imports: [
     BrowserModule
