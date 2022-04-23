393f9f32c9989b649dbedc0275cdfcdaab2164bc
Lloni <jonathan.pacheco@gmail.com>
2022-04-23 15:27:16 -0400

    slider creado, faltan los estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HeroComponent
+    HeroComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
