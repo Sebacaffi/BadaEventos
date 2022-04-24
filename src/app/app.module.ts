b3a713dc7469370d25fb542f702aa071b55ed6c1
NIcolas Cañete <ncanete@trust.lat>
2022-04-24 18:58:08 -0400

    resolucion conflictos en rama

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { SliderComponent } from './components/slider/slider.component';
 // import { FormularioContactoComponent } from './formulario-contacto/formulario-contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeroComponent,
+    SliderComponent
     // FormularioContactoComponent
   ],
   imports: [
