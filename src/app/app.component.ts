0cc7dc4df1878111b4f0e12692076ab58d413c0a
NIcolas Cañete <ncanete@trust.lat>
2022-04-29 12:14:49 -0400

    heroku deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ContactoService } from './services/contacto.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers:[ContactoService]
 })
 export class AppComponent {
   title = 'badaEventos';
+
 }
