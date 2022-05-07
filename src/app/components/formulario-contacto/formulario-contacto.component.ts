548c2193686f2480601f18bd33ec8b06fb6cffb5
sebaCaffi <sebastian.caffi@gmail.com>
2022-05-07 11:40:32 -0400

    alertas

--- src/app/components/formulario-contacto/formulario-contacto.component.ts
+++ src/app/components/formulario-contacto/formulario-contacto.component.ts
@@ -2,6 +2,8 @@ import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { ContactoService } from 'src/app/services/contacto.service';
 
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-formulario-contacto',
   templateUrl: './formulario-contacto.component.html',
@@ -24,4 +26,11 @@ export class FormularioContactoComponent implements OnInit {
     )
   }
 
+  alertaEnvio(){
+    Swal.fire(
+      'Mensaje enviado!',
+      'Lo contactaremos en breve!',
+      'success'
+    )
+  }
 }
