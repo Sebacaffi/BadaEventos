05e1589cd6de02f0e9fa06f136b90be7f0784186
sebaCaffi <sebastian.caffi@gmail.com>
2022-05-12 20:24:46 -0400

    nuevo push

--- src/app/evento/evento.module.ts
+++ src/app/evento/evento.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AcordeonComponent } from './acordeon/acordeon.component';
+import { FrameDatosComponent } from './frame-datos/frame-datos.component';
 
 
 
 @NgModule({
   declarations: [
-    AcordeonComponent
+    AcordeonComponent,
+    FrameDatosComponent
   ],
   imports: [
     CommonModule
