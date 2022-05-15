e1ce769236f68c0818e50b5cf68860f90c835abe
sebaCaffi <sebastian.caffi@gmail.com>
2022-05-15 18:23:55 -0400

    carpetas

--- src/app/evento/evento.module.ts
+++ src/app/evento/evento.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AcordeonComponent } from './acordeon/acordeon.component';
-import { FrameDatosComponent } from './frame-datos/frame-datos.component';
+import { AcordeonComponent } from './components/acordeon/acordeon.component';
+import { FrameDatosComponent } from './components/frame-datos/frame-datos.component';
 
 
 
