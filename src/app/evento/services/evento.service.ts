3b1daa3fee61f8fe56f88c6ccac1e485204cd259
NIcolas Cañete <ncanete@trust.lat>
2022-05-22 17:09:05 -0400

    ngFor acordeon

--- src/app/evento/services/evento.service.ts
+++ src/app/evento/services/evento.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Prevent } from '../models/evento.model';
+import { Age } from '../models/evento.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -15,6 +16,11 @@ export class EventoService {
 
   getEvents(): Observable<Prevent[]> {
     return this.http.get<Prevent[]>(this.url);
+  }
+
+  getAge(): Observable<Age[]> {
+    let endpoint = "public"
+    return this.http.get<Age[]>(this.url+endpoint);
   }
 
   // getEventById(id: number): Observable<Prevent[]> {
