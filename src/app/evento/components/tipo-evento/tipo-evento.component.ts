05cf45b227dde77128ffe340da21860e9570838e
sebaCaffi <sebastian.caffi@gmail.com>
2022-06-05 17:32:38 -0400

    comentarios code

--- src/app/evento/components/tipo-evento/tipo-evento.component.ts
+++ src/app/evento/components/tipo-evento/tipo-evento.component.ts
@@ -15,15 +15,20 @@ export class TipoEventoComponent implements OnInit {
   constructor(private eventService: EventoService) {
    }
 
+  //-----SERVICIOS QUE SE CARGAN AL INICIAR LA PAGE-----//
 
   ngOnInit(): void {
 
+    //obtenemos los pre-eventos de la API
     this.eventService.getEvents().subscribe((eventsFromApi: Prevent[]) =>
       this.resultList = eventsFromApi
     ), error => console.error(error)
 
   }
 
+  //-------FUNCIONES------//
+
+  //función para obtener los pre-eventos de la API filtrando por ID
   getEventById(id: number) {
 
     this.resultList.forEach(event => {
--- src/app/home/services/contacto.service.ts
+++ src/app/home/services/contacto.service.ts
@@ -7,10 +7,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export class ContactoService {
 
+  //-----URL EDPOINT API-----//
   url = 'https://badaeventos.herokuapp.com/api/contact/'
 
   constructor(private http: HttpClient) {   }
 
+  //Post para enviar el formulario de contacto
   sendContactForm(data) {
 
     return this.http.post(this.url, data)
