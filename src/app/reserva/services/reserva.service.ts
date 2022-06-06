05159943d80108d137467da202a3d5c50faae43e
NIcolas Cañete <ncanete@trust.lat>
2022-06-05 20:00:59 -0400

    cambios

--- src/app/reserva/services/reserva.service.ts
+++ src/app/reserva/services/reserva.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservaService {
 
-  constructor() { }
+  url = 'https://badaeventos.herokuapp.com/market/customers/'
+
+  constructor(private http: HttpClient) { }
+
+  sendEventCustomer(data) {
+    return this.http.post(this.url, data)
+  }
 }
