3716ec5fd0fc4c3e975e686c6e187bbabca4a0bc
sebaCaffi <sebastian.caffi@gmail.com>
2022-06-13 17:27:59 -0400

    cambios paypal a TS

--- src/app/reserva/components/detalle-reserva/detalle-reserva.component.ts
+++ src/app/reserva/components/detalle-reserva/detalle-reserva.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ReservaService } from '../../services/reserva.service';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+
+declare var paypal;
 
 @Component({
   selector: 'app-detalle-reserva',
@@ -9,7 +11,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./detalle-reserva.component.scss']
 })
 export class DetalleReservaComponent implements OnInit{
-
   //----------VARIABLES USADAS EN LAS FUNCIONES-------------//
 
   errorMessage = '';
@@ -17,7 +18,6 @@ export class DetalleReservaComponent implements OnInit{
   Currency = "0";
   totalUSD = 0;
   CurrencyUSD = "0";
-  ResultPago: string;
 
   //------------------OBJETOS-------------------------//
 
@@ -51,8 +51,40 @@ export class DetalleReservaComponent implements OnInit{
     //se guarda ID de evento creado en persistencia
     this.formulario.event_booking = this.eventoPago.search_id
 
-    this.ResultPago = JSON.parse(localStorage.getItem('ResultPago'))
-    console.log(this.ResultPago)
+    // Render the PayPal button into #paypal-button-container
+    paypal.Buttons({
+
+      //ESTILOS
+      style: {
+        color: 'blue',
+        shape: 'pill',
+        label: 'pay',
+        height: 40
+      },
+
+      // Se setea el valor del evento para ser pagado en paypal
+      createOrder: function (data, actions) {
+          return actions.order.create({
+              purchase_units: [{
+                  amount: {
+                      value: localStorage.getItem('totalUSD'),
+                      currency_code: 'USD'
+                  }
+              }]
+          });
+      },
+
+      // si la transaccion finaliza me mandan el post al CUSTOMERS
+      onApprove: function (data, actions) {
+          return actions.order.capture().then(function (orderData) { 
+              // Successful capture! For demo purposes:
+              var transaction = orderData.purchase_units[0].payments.captures[0];
+              alert('Transaction ' + transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+              localStorage.setItem('transaction', JSON.stringify(transaction.status));
+          });
+      }
+
+    }).render('#paypal-button-container');
 
   }
 
