9da6a241aacbdbef3d4d06cd8b5aea7170f8e08f
NIcolas Cañete <ncanete@trust.lat>
2022-06-16 20:23:56 -0400

    cambios

---
+++ src/app/admin/login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  url = "https://badaeventos.herokuapp.com/account/token/"
+
+  constructor(private http: HttpClient) { }
+
+  sendLogin(data) {
+    return this.http.post(this.url, data)
+  }
+}
